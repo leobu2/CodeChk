a0d8c47488342ac43db20763e65536d4543301e5
leobu2 <allen.cisco@gmail.com>
2018-08-14 14:02:24 +0800

    add ip info

--- myJS.js
+++ myJS.js
@@ -3,7 +3,7 @@ var region_name; //"Taipei City"
 var country_name;//"Taiwan"
 var latitude;//25.0478
 var longitude;	//121.5318
-
+var ip;//192.168.1.1
 function getPosition(){
 	$.getJSON("https://ip-api.io/json/",
 	
@@ -13,7 +13,7 @@ function getPosition(){
 		country_name = result.country_name;
 		latitude = result.latitude;
 		longitude = result.longitude;
-		
+		ip = result.ip;
     });
 }	
 
@@ -21,7 +21,7 @@ function myFunInput(UserEnter,title){
 
 	appUrl = "https://script.google.com/macros/s/AKfycbxzZTtlQrGpENBGfgF50cwMwq3jgf67XBsFmNz5jQTSn8TTw15V/exec";
 	var Today = new Date();
-	var data = [[Today,title,UserEnter.toUpperCase(),region_name,country_name,latitude,longitude]];
+	var data = [[Today,title,UserEnter.toUpperCase(),region_name,country_name,latitude,longitude,ip]];
 	var parameter = {
 	url: "https://docs.google.com/spreadsheets/d/1trX7v1VnJi15k_oRFX4s2fCjVDKUqHCH3mXw5WnSv-A/edit?usp=sharing",
 	name: "工作表1",
