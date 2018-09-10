53995de810121f7a180d7e6ff16ab55f481ac76e
leobu2 <allen.cisco@gmail.com>
2018-09-10 14:17:07 +0800

    with cookie testing

---
+++ msJS-2.js
@@ -0,0 +1,70 @@
+
+var region_name; //"Taipei City"
+var country_name;//"Taiwan"
+var latitude;//25.0478 (緯度)
+var longitude;	//121.5318 (經度)
+var ip;//192.168.1.1
+
+function getPosition(){}//KEEP THIS FUNCTION FOR HTML PAGE USE
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 30);
+       }
+    }
+}
+
+function myFunInput(UserEnter,title){
+		
+	appUrl = "https://script.google.com/macros/s/AKfycbxzZTtlQrGpENBGfgF50cwMwq3jgf67XBsFmNz5jQTSn8TTw15V/exec";
+	var Today = new Date();
+	
+	$.getJSON("https://ip-api.io/json/",	
+    		function(result) {
+			console.log(result);
+			region_name = result.region_name;		
+			country_name = result.country_name;
+			latitude = result.latitude;
+			longitude = result.longitude;
+			ip = result.ip;
+			var data = [[Today,title,UserEnter.toUpperCase(),region_name,country_name,latitude,longitude,ip]];
+			var parameter = {
+				url: "https://docs.google.com/spreadsheets/d/1trX7v1VnJi15k_oRFX4s2fCjVDKUqHCH3mXw5WnSv-A/edit?usp=sharing",
+				name: "工作表1",
+				data: data.toString(),
+				row: data.length,
+				column: data[0].length,
+				insertType:"bottom",
+			};
+			$.get(appUrl, parameter);
+    	});	
+	 
+}
